981eb53e9bf9536be728e14d93daedbc4c5ebe09
henrypigg <henry.pigg@gmail.com>
2022-10-26 22:44:28 -0700

    Initial commit

---
+++ test/fibula-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FibulaCdk from '../lib/fibula-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fibula-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FibulaCdk.FibulaCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
