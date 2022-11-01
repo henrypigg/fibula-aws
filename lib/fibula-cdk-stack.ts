03c4d81f714395321e8c322e5133692e4e35dc72
henrypigg <henry.pigg@gmail.com>
2022-10-31 19:49:41 -0700

    Added cdk files

--- lib/fibula-cdk-stack.ts
+++ lib/fibula-cdk-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { FibulaLambdas } from './fibula-lambdas';
+import {aws_s3 as s3} from 'aws-cdk-lib'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class FibulaCdkStack extends cdk.Stack {
+  readonly fibulaLambdas = FibulaLambdas;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, 'CDKBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+    // Lambdas
+    
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FibulaCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // SNS Topic
+
+
+    // API
+
+
   }
 }
---
+++ lib/fibula-lambdas.ts
@@ -0,0 +1,12 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class FibulaLambdas extends Construct {
+  constructor(scope: Construct, id: string,) {
+    super(scope, id);
+
+
+
+  }
+}
