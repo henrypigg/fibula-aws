494943d0451fa7cece6b3d69135229d185a9a15a
henrypigg <henry.pigg@gmail.com>
2022-12-07 14:14:43 -0800

    Created pipeline

--- bin/fibula-cdk.ts
+++ bin/fibula-cdk.ts
@@ -2,9 +2,15 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { FibulaCdkStack } from '../lib/fibula-cdk-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
+new PipelineStack(app, 'FibulaPipelineStack', {
+  env: { account: '374516698077', region: 'us-east-1'}
+});
+/**
 new FibulaCdkStack(app, 'FibulaCdkStack', {
+  env: { account: '374516698077', region: 'us-east-1'}
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +24,6 @@ new FibulaCdkStack(app, 'FibulaCdkStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+//});
+
+app.synth();
--- lib/fibula-cdk-stack.ts
+++ lib/fibula-cdk-stack.ts
@@ -2,25 +2,34 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { FibulaLambdas } from './fibula-lambdas';
 import {aws_s3 as s3} from 'aws-cdk-lib'
+import { Subscription, Topic } from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class FibulaCdkStack extends cdk.Stack {
-  readonly fibulaLambdas = FibulaLambdas;
 
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+export class FibulaCdkStack extends cdk.Stack {
+  readonly sendEnrollmentRequestTopic: Topic;
+  readonly fibulaLambdas: FibulaLambdas;
+
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    new s3.Bucket(this, 'CDKBucket', {
-      versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true
-    });
-    // Lambdas
-    
-
     // SNS Topic
+    this.sendEnrollmentRequestTopic = new Topic(this, 'SendEnrollmentRequestTopic', {
+      displayName: 'Send enrollment request email',
+      topicName: 'send-enrollment-request',
+    });
 
+    this.sendEnrollmentRequestTopic.addSubscription(
+      new EmailSubscription('henry.pigg@gmail.com')
+    )
 
+    // Lambdas
+    this.fibulaLambdas = new FibulaLambdas(this, 'Lambdas', {
+      topic: this.sendEnrollmentRequestTopic
+    });
+    
     // API
 
 
