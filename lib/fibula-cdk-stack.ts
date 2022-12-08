3289f091ca1b0ce7af911c1358d48a2fa1f0e163
henrypigg <henry.pigg@gmail.com>
2022-12-07 17:50:03 -0800

    Revert "Created pipeline"
    This reverts commit 494943d0451fa7cece6b3d69135229d185a9a15a.

--- lib/fibula-cdk-stack.ts
+++ lib/fibula-cdk-stack.ts
@@ -2,34 +2,25 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { FibulaLambdas } from './fibula-lambdas';
 import {aws_s3 as s3} from 'aws-cdk-lib'
-import { Subscription, Topic } from 'aws-cdk-lib/aws-sns';
-import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-
-
 export class FibulaCdkStack extends cdk.Stack {
-  readonly sendEnrollmentRequestTopic: Topic;
-  readonly fibulaLambdas: FibulaLambdas;
+  readonly fibulaLambdas = FibulaLambdas;
 
-  constructor(scope: Construct, id: string, props: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // SNS Topic
-    this.sendEnrollmentRequestTopic = new Topic(this, 'SendEnrollmentRequestTopic', {
-      displayName: 'Send enrollment request email',
-      topicName: 'send-enrollment-request',
+    new s3.Bucket(this, 'CDKBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
     });
-
-    this.sendEnrollmentRequestTopic.addSubscription(
-      new EmailSubscription('henry.pigg@gmail.com')
-    )
-
     // Lambdas
-    this.fibulaLambdas = new FibulaLambdas(this, 'Lambdas', {
-      topic: this.sendEnrollmentRequestTopic
-    });
     
+
+    // SNS Topic
+
+
     // API
 
 
--- lib/fibula-lambdas.ts
+++ lib/fibula-lambdas.ts
@@ -1,29 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
-import { Function } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-import { Runtime, Code } from 'aws-cdk-lib/aws-lambda'
-import { Topic } from 'aws-cdk-lib/aws-sns';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-interface FibulaLambdasProps {
-    topic: Topic
-}
-
 export class FibulaLambdas extends Construct {
-    readonly sendEnrollmentRequestLambda: Function;
+  constructor(scope: Construct, id: string,) {
+    super(scope, id);
 
-    constructor(scope: Construct, id: string, props: FibulaLambdasProps) {
-        super(scope, id);
 
-        this.sendEnrollmentRequestLambda = new Function(scope, 'SendEnrollmentRequestLambda', {
-            runtime: Runtime.PYTHON_3_9,
-            code: Code.fromAsset("resources"),
-            handler: "send_email_handler.lambda_handler",
-            environment: {
-                TOPIC_ARN: props.topic.topicArn
-            }
-        });
 
-        props.topic.grantPublish(this.sendEnrollmentRequestLambda);
   }
 }
