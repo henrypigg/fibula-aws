53a24421e4b944a56d62e9ef97757e78b5fb0fed
henrypigg <henry.pigg@gmail.com>
2023-03-22 19:51:23 -0700

    Updated get installer handler to support windows

--- resources/lambdas/get_installer_handler.py
+++ resources/lambdas/get_installer_handler.py
@@ -6,22 +6,24 @@ import boto3
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3')
-    print(event)
+    platform = event.get('pathParameters', {}).get('platform', 'macos')
     # Get all objects in the bucket
-    response = s3.list_objects_v2(Bucket=os.environ['BUCKET_NAME'])
+    response = s3.list_objects_v2(Bucket=os.environ['BUCKET_NAME'], Prefix=f'{platform}/')
 
     # Paginate if necessary
     while response['IsTruncated']:
         response['Contents'].extend(
             s3.list_objects_v2(
                 Bucket=os.environ['BUCKET_NAME'],
-                PaginationConfig={'StartingToken': response['NextToken']}
+                PaginationConfig={'StartingToken': response['NextToken']},
+                Prefix=f'{platform}/'
             )['Contents']
         )
 
     if response['KeyCount'] > 0:
         # Get the latest version
-        version = sorted(response['Contents'], key=lambda x: x['Key'], reverse=True)[0]['Key'].split('/')[0]
+        versions = map(lambda x: x['Key'].split('/')[1], response['Contents'])
+        latest_version = sorted(versions, reverse=True)[0]
 
         # return presigned url for 12 hours
         return {
@@ -35,7 +37,7 @@ def lambda_handler(event, context):
                 ClientMethod='get_object',
                 Params={
                     'Bucket': os.environ['BUCKET_NAME'],
-                    'Key': f'{version}/femr-x64-{version}.pkg'
+                    'Key': f'{platform}/{latest_version}/femr-x64-{latest_version}.{"pkg" if platform == "macos" else "exe"}'
                 },
                 ExpiresIn=43200
             )
